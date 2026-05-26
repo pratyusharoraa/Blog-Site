c689ba3c83ee35f88ceab2de300675df546438c3
pratyusharoraa <pratyusharora7@gmail.com>
2026-05-26 16:37:55 +0530

    made home.html

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ posts: list[dict] = [
     }
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html")
 
 @app.get("/api/posts")
 def get_posts():
