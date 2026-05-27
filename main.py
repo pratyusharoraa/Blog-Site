083bb5dec18c56aeab3ff218d73caf9f46ba16ff
pratyusharoraa <pratyusharora7@gmail.com>
2026-05-27 16:28:05 +0530

    Added Request and Response validation with Pydantic Schemas - 2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -52,10 +53,22 @@ def post_page(request: Request, post_id: int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model = list[PostResponse])
 def get_posts():
     return posts
 
+@app.post("/api/posts", response_model = PostResponse, status_code = status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025"
+    }
+    posts.append(new_post)
+    return new_post
 
 @app.get("/api/posts/{post_id}")
 def get_post(post_id: int):
